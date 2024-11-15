8f185f5c507eff5d624a4bcedb5fae8c486afab9
guadarramaggHybridge <gerardo2402004@hybridge.education>
2024-11-14 19:57:41 -0600

    Create suma.py

---
+++ suma.py
@@ -0,0 +1,9 @@
+# Solicitar dos números al usuario
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+# Sumar los números
+resultado = num1 + num2
+
+# Imprimir el resultado
+print("La suma de los dos números es:", resultado)
